18450b50c5d98a54ac715f77a2cc5e9c44d31d04
Sarmad Ali <132395342+sarmadalikhanofficial@users.noreply.github.com>
2025-02-25 09:57:12 +0500

    Create firstClass.java

---
+++ firstClass.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+public class myFirstClass {
+	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		int age = input.nextInt();
+		System.out.println("Your age is: "+age);
+	}
+}
+
+
